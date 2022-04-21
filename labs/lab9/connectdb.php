9c7fa9992c0e52dfcfe4ba454daf514429956062
BuildTools <unconfigured@null.spigotmc.org>
2022-04-21 16:49:50 -0400

    scrub

--- labs/lab9/connectdb.php
+++ labs/lab9/connectdb.php
@@ -1,8 +1,8 @@
 <?php
-$server = "sql107.epizy.com";
-$database = "epiz_30981357_lab8";
-$username = "epiz_30981357";
-$pword = "grZBUUSEcco";
+$server = "";
+$database = "";
+$username = "";
+$pword = "";
 $db_conn = new mysqli($server, $username, $pword, $database);
 if($db_conn->connect_error){
     die("Fatal Err!");
